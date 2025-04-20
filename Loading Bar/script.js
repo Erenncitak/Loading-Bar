680a14f9c18c18f25ad767a24c116f4ba72f1a89
Eren <erenncitak@gmail.com>
2025-04-20 17:54:00 +0300

    Loading Bar
    Bu küçük proje, HTML, CSS ve JavaScript kullanarak basit bir yüklenme animasyonu (loading bar) oluşturmayı amaçlamaktadır.

---
+++ Loading Bar/script.js
@@ -0,0 +1,16 @@
+const counter = document.querySelector('.counter');
+const loadingBarFront = document.querySelector('.loadingBarFront');
+
+let number = 0;
+
+function updateNumber() {
+    counter.textContent = number + '%';
+    loadingBarFront.style.width = number + '%';
+    number++;
+    
+    if (number <= 100) {
+        setTimeout(updateNumber, 15);
+    }
+}
+
+updateNumber();
